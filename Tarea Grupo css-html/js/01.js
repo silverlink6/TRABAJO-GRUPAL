859c9259a50a96506ee6c0725295861f1a7b13b2
silverlink6 <120565246+silverlink6@users.noreply.github.com>
2023-03-26 11:43:04 +0100

    Add files via upload

---
+++ Tarea Grupo css-html/js/01.js
@@ -0,0 +1,16 @@
+
+// Variable para introducir los datos
+let data = document.
+getElementById('input');
+
+//Variable para mostrar resulado
+let result = document.
+getElementById('result');
+
+// te devuelve lo que has escrito con codigo en HTML 
+// cuando le das al botón run
+function run(){
+    result.innerHTML=data.
+    value;
+}
+
